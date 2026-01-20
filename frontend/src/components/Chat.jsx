471972876d8d87b9cafd86f9a0198d437eef8a27
SUDAR2005 <sudarsettaiyan@gmail.com>
2026-01-20 19:01:20 +0530

    textarea based input added

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -30,7 +30,10 @@ const Chat = () => {
   const handleSend = () => {
     if (!query.trim()) return;
     console.log("Send:", query);
-    setMessages(...messages, {"time": Date.now(), "message": query})
+    setMessages((prev) => [
+      ...prev,
+      { time: Date.now(), message: query }
+    ]);
     setQuery("");
     inputRef.current?.focus();
   };
@@ -63,36 +66,82 @@ const Chat = () => {
           `}
         >
 
-          <div className="flex flex-col items-center gap-6">
-            {messages.length == 0 ? <div className="text-3xl font-semibold text-center">
-              What's on your camera today? Let's find !!!
-            </div>: "" }
+          <div
+            className={`flex flex-col gap-6 transition-all duration-500 items-center
+              ${messages.length === 0
+                ? "w-2xl"
+                : "w-full max-w-5xl"}
+            `}
+          >
+            {messages.length === 0 ? (
+              <div className="text-3xl font-semibold text-center">
+                What's on your camera today? Let's find !!!
+              </div>
+              ) : (
+              <div className="w-full flex flex-col gap-3 px-4 max-h-[70vh] overflow-y-auto">
+                {messages.map((msg, index) => (
+                  <div
+                    key={msg.time || index}
+                    className={`self-end px-4 py-2 rounded-xl max-w-md
+                      ${isDark ? "bg-[#2f2f2f] text-white" : "bg-gray-200 text-black"}
+                    `}
+                  >
+                    {msg.message}
+                  </div>
+                ))}
+              </div>
+            )}
 
             {/* Search Input */}
-            <div className={`w-2xl relative`}>
+            <div className={`${messages.length === 0 ? "w-2xl" : "w-4xl"} transition-[width] duration-200 ease-linear relative`}>
               {query ? (
                 <FiSend
-                  className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-500 text-lg cursor-pointer"
+                  className="text-3xl absolute right-4 bottom-0.5 pr-2 -translate-y-1/2 text-gray-500 cursor-pointer"
                   onClick={handleSend}
                 />
               ) : (
                 <FaSearch className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-400 text-lg" />
               )}
-              <input
-                type="text"
-                placeholder="Query your video recordings..."
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-                onKeyDown={(e) => e.key === "Enter" && handleSend()}
+              <textarea
                 ref={inputRef}
-                className={`w-full px-4 py-3 pr-12 rounded-xl border
+                value={query}
+                placeholder="Query your video recordings..."
+                rows={1}
+                onChange={(e) => {
+                  setQuery(e.target.value);
+
+                  // Reset height
+                  e.target.style.height = "auto";
+
+                  const maxHeight = 160;
+
+                  if (e.target.scrollHeight > maxHeight) {
+                    e.target.style.height = `${maxHeight}px`;
+                    e.target.style.overflowY = "auto";
+                  } else {
+                    e.target.style.height = `${e.target.scrollHeight}px`;
+                    e.target.style.overflowY = "hidden";
+                  }
+                }}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter" && !e.shiftKey) {
+                    e.preventDefault();
+                    handleSend();
+                    e.target.style.height = "auto";
+                    e.target.style.overflowY = "hidden";
+                  }
+                }}
+                className={`w-full resize-none px-4 py-3 pr-12 rounded-xl border
                   ${
                     isDark
                       ? "bg-[#1f1f1f] border-[#333] text-white"
                       : "bg-white border-gray-300"
                   }
-                  focus:outline-none focus:ring-2 focus:ring-gray-500`}
+                  max-h-40
+                  focus:outline-none focus:ring-2 focus:ring-gray-500
+                `}
               />
+
             </div>
           </div>
         </div>
